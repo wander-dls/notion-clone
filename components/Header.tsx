2acac4e4105112f5e7a86de524980b9588c0b417
wander-dls <wd.delossantos@gmail.com>
2025-04-01 13:24:02 -0400

    first commit, clerk auth implemented

---
+++ components/Header.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import { SignedIn, SignedOut, SignInButton, UserButton, useUser } from "@clerk/nextjs";
+import { User } from "lucide-react";
+
+const Header = () => {
+  const { user } = useUser();
+  return (
+    <>
+      <div className="flex items-center justify-between p-5">
+        {user && (
+          <h1>
+            {user.firstName} {`'s`} Space
+          </h1>
+        )}
+
+
+        {/* Breadcrumbs */}
+
+
+        <div>
+          <SignedOut>
+            <SignInButton mode="modal">
+              Sign in
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+              <UserButton />    
+          </SignedIn>
+        </div>
+      </div>
+
+    </>
+  )
+}
+export default Header
